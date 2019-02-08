5d3654ca4692ac22039ec8c19dce4d496b72f9e7
skarimo <karimovj@gmail.com>
2019-02-08 09:04:34 -0500

    fixed deprecated function

--- DFT-frontend/src/photo.js
+++ DFT-frontend/src/photo.js
@@ -3,7 +3,7 @@ function loadCamera() {
   if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
       navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-          video.src = window.URL.createObjectURL(stream);
+          video.srcObject = stream
           localstream = stream
           // video.play();
       });
